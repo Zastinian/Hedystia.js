925ec02d867fcb11797aed66e46f8eefc8b280df
Zastinian <zastinian@gmail.com>
2022-07-11 20:37:57 -0500

    Actualizado intents

--- Util/Intents.js
+++ Util/Intents.js
@@ -1,33 +1,78 @@
-const Bits = require("./Bits");
+"use strict";
 
-class Intents extends Bits() {}
+class Intents extends null {
+  static parse(intents) {
+    if (typeof intents === "number") return intents;
+    if (!Array.isArray(intents))
+      throw new Error("Los intents deben ser un array o un número");
 
-Intents.FLAGS = {
-  Servidores: 1 << 0,
-  Miembros: 1 << 1,
-  Baneos: 1 << 2,
-  Emojis: 1 << 3,
-  Integraciones: 1 << 4,
-  Webhooks: 1 << 5,
-  Invitaciones: 1 << 6,
-  Voz: 1 << 7,
-  Presencias: 1 << 8,
-  Mensajes: 1 << 9,
-  Reacciones: 1 << 10,
-  Escribiendo: 1 << 11,
-  MensajeDirecto: 1 << 12,
-  ReaccionesPrivadas: 1 << 13,
-  EscribiendoPrivado: 1 << 14,
-  ContenidoDelMensaje: 1 << 15,
-  EventosProgramados: 1 << 16,
-  ConfiguracionDeAutoModeracion: 1 << 20,
-  AccionesDeAutoModeracion: 1 << 21,
-};
-
-Intents.default =
-  Intents.FLAGS.Servidores |
-  Intents.FLAGS.Miembros |
-  Intents.FLAGS.ContenidoDelMensaje |
-  Intents.FLAGS.Emojis;
+    let finalIntents = 0;
+    for (const intent of intents) {
+      switch (intent) {
+        case "Servidores":
+          finalIntents |= 1 << 0;
+          break;
+        case "Miembros":
+          finalIntents |= 1 << 1;
+          break;
+        case "Baneos":
+          finalIntents |= 1 << 2;
+          break;
+        case "Emojis":
+          finalIntents |= 1 << 3;
+          break;
+        case "Integraciones":
+          finalIntents |= 1 << 4;
+          break;
+        case "Webhooks":
+          finalIntents |= 1 << 5;
+          break;
+        case "Invitaciones":
+          finalIntents |= 1 << 6;
+          break;
+        case "Voz":
+          finalIntents |= 1 << 7;
+          break;
+        case "Presencias":
+          finalIntents |= 1 << 8;
+          break;
+        case "Mensajes":
+          finalIntents |= 1 << 9;
+          break;
+        case "Reacciones":
+          finalIntents |= 1 << 10;
+          break;
+        case "Escribiendo":
+          finalIntents |= 1 << 11;
+          break;
+        case "MensajeDirecto":
+          finalIntents |= 1 << 12;
+          break;
+        case "ReaccionesPrivadas":
+          finalIntents |= 1 << 13;
+          break;
+        case "EscribiendoPrivado":
+          finalIntents |= 1 << 14;
+          break;
+        case "ContenidoDelMensaje":
+          finalIntents |= 1 << 15;
+          break;
+        case "EventosProgramados":
+          finalIntents |= 1 << 16;
+          break;
+        case "ConfiguracionDeAutoModeracion":
+          finalIntents |= 1 << 20;
+          break;
+        case "AccionesDeAutoModeracion":
+          finalIntents |= 1 << 21;
+          break;
+        default:
+          finalIntents |= 0;
+          break;
+      }
+    }
+    return finalIntents;
+  }
+}
 
 module.exports = Intents;
