fd0b44b94b481ca4722de31ed53bd6954cf80df6
Zastinian <zastinian@gmail.com>
2022-07-11 22:22:35 -0500

    Añadido MensajeEliminado

---
+++ Action/MESSAGE_DELETE.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports.handle = (client, data) => {
+  const channel = client.channels.cache.get(data.channel_id);
+  const message = channel?.messages.cache.get(data.id);
+  if (message) message.deleted = true;
+  client.emit("MensajeEliminado", message ?? data);
+};
