a7666844105d5dab4c1b6a83d99ba889d971c1e9
Zastinian <zastinian@gmail.com>
2022-07-11 21:35:52 -0500

    Añadido ClientUser

---
+++ Structure/ClientUser.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const User = require("./User");
+
+class ClientUser extends User {
+  constructor(client, data) {
+    super(client, data);
+    super.parseData(data);
+  }
+
+  setAFK(afk = true) {
+    this.setPresence({ afk });
+  }
+
+  setActivity(name, type = 0) {
+    this.setPresence({
+      activities: [
+        {
+          type,
+          name,
+        },
+      ],
+    });
+  }
+
+  setPresence({ activities = [], status = "online", afk = false }) {
+    const data = {
+      op: 3,
+      d: {
+        activities,
+        status,
+        afk,
+        since: null,
+      },
+    };
+    this.client.ws.connection?.send(JSON.stringify(data));
+  }
+}
+
+module.exports = ClientUser;
