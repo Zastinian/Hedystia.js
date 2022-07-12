31144505afb11868b5ff4ea0b2350ee114f42cd6
Zastinian <zastinian@gmail.com>
2022-07-11 21:56:07 -0500

    Actualizado ChannelManager

---
+++ Manager/ChannelManager.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const TextChannel = require("../Structure/TextChannel");
+const LimitedMap = require("../Util/LimitedMap");
+const Requester = require("../Util/Requester");
+
+class ChannelManager {
+  constructor(client, limit) {
+    this.cache = new LimitedMap(limit);
+    this.client = client;
+  }
+
+  async fetch(id) {
+    if (this.cache.has(id)) return this.cache.get(id);
+
+    const data = await Requester.create(
+      this.client,
+      `/channels/${id}`,
+      "GET",
+      true
+    );
+    let channel = null;
+    switch (data.type) {
+      case 0:
+        channel = new TextChannel(
+          this.client,
+          data,
+          this.client.guilds.cache.get(data.guild_id)
+        );
+        break;
+    }
+
+    this.cache.set(channel.id, channel);
+    return channel;
+  }
+}
+
+module.exports = ChannelManager;
