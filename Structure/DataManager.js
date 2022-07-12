c844554c54748bcb4107283786ff5743e93e2930
Zastinian <zastinian@gmail.com>
2022-07-11 21:14:56 -0500

    Añadido DataManager

---
+++ Structure/DataManager.js
@@ -0,0 +1,19 @@
+"use strict";
+
+class DataManager {
+  constructor(client) {
+    this.client = client;
+  }
+
+  _clone() {
+    return Object.assign(Object.create(this), this);
+  }
+
+  _update(data, ...params) {
+    const clone = this._clone();
+    this.parseData?.(data, params);
+    return clone;
+  }
+}
+
+module.exports = DataManager;
