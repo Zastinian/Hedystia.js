bb810585e811539a6de3d8eceda2c40e27fd94d9
Zastinian <zastinian@gmail.com>
2022-07-11 22:17:24 -0500

    Añadido ChannelDelete

---
+++ Action/CHANNEL_DELETE.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const TextChannel = require("../Structure/TextChannel");
+
+module.exports.handle = (client, data) => {
+  switch (data.type) {
+    case 0: {
+      const channel = new TextChannel(
+        client,
+        data,
+        client.guilds.cache.get(data.guild_id)
+      );
+      client.channels.cache.delete(data.id);
+      client.emit("CanalEliminado", channel);
+      break;
+    }
+  }
+};
