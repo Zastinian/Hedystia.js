236a91cb0fc27c96d3ff56c4c86216c3cf3a3f06
Zastinian <zastinian@gmail.com>
2022-07-11 21:13:18 -0500

    Actualizado DiscordEndPoints

--- Constant/DiscordEndpoints.js
+++ Constant/DiscordEndpoints.js
@@ -4,14 +4,12 @@ module.exports = {
   gatewayUrl: (version, encoding) =>
     `wss://gateway.discord.gg/?v=${version}&encoding=${encoding}`,
   apiUrl: (version) => `https://discord.com/api/v${version}`,
-
   userBanner: (userId, hash, size, format = "png") =>
     `https://cdn.discordapp.com/banners/${userId}/${hash}.${format}?size=${size}`,
   userAvatar: (userId, hash, size, format = "png") =>
     `https://cdn.discordapp.com/avatars/${userId}/${hash}.${format}?size=${size}`,
   userDefaultAvatar: (discriminator) =>
     `https://cdn.discordapp.com/embed/avatars/${discriminator % 5}.png`,
-
   guildIcon: (guildId, hash, size, format = "png") =>
     `https://cdn.discordapp.com/icons/${guildId}/${hash}.${format}?size=${size}`,
   guildSplash: (guildId, hash, size, format = "png") =>
