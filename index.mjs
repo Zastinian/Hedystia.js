2bde63b80d26b37ee222e4f264b728dd266c0025
Zastinian <zastinian@gmail.com>
2022-07-11 22:27:04 -0500

    Añadido module index

---
+++ index.mjs
@@ -0,0 +1,25 @@
+import Esmile from "./index.js";
+
+export const {
+  Client,
+  ClientUser,
+  CommandInteraction,
+  DataManager,
+  Guild,
+  GuildMember,
+  Invite,
+  Message,
+  MessageEmbed,
+  Role,
+  TextChannel,
+  User,
+  ChannelManager,
+  ChannelMessageManager,
+  EmojiManager,
+  GuildChannelManager,
+  GuildManager,
+  RoleManager,
+  UserManager,
+  Utils,
+  version,
+} = Esmile;
