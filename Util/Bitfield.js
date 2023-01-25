e04af9d4c079069189e40d7b5d575b6909ba4014
Zastinian <zastinian@gmail.com>
2023-01-24 18:50:19 -0600

    fix: Bitfield
    Text and Flags

--- Util/Bitfield.js
+++ Util/Bitfield.js
@@ -1,78 +1,78 @@
-const BitfieldInvalid = require("../Errors/BitfieldInvalid")
+const BitfieldInvalid = require("../Errors/BitfieldInvalid");
 
 class Bitfield {
   constructor(bit = this.constructor.defaultBit) {
-    this.bitfield = this.constructor.resolve(bit)
+    this.bitfield = this.constructor.resolve(bit);
   }
 
   any(...bit) {
-    return (this.bitfield & this.constructor.resolve(bit)) !== this.constructor.defaultBit
+    return (this.bitfield & this.constructor.resolve(bit)) !== this.constructor.defaultBit;
   }
 
   has(...bit) {
-    bit = this.constructor.resolve(bit)
-    return (this.bitfield & bit) === bit
+    bit = this.constructor.resolve(bit);
+    return (this.bitfield & bit) === bit;
   }
 
   add(...bit) {
-    bit = this.constructor.resolve(bit)
-    this.bitfield |= bit
-    return this
+    bit = this.constructor.resolve(bit);
+    this.bitfield |= bit;
+    return this;
   }
 
   remove(...bits) {
-    let total = this.constructor.defaultBit
+    let total = this.constructor.defaultBit;
     for (const bit of bits) {
-      total |= this.constructor.resolve(bit)
+      total |= this.constructor.resolve(bit);
     }
 
-    if (Object.isFrozen(this)) return new this.cache.constructor(total & ~total)
-    this.bitfield &= ~total
-    return this
+    if (Object.isFrozen(this)) return new this.cache.constructor(total & ~total);
+    this.bitfield &= ~total;
+    return this;
   }
 
   toArray() {
-    return Object.keys(this.constructor.FLAGS).filter((o) => this.has(o))
+    return Object.keys(this.constructor.Flags).filter((o) => this.has(o));
   }
 
   toString() {
-    return this.bitfield.toString()
+    return this.bitfield.toString();
   }
 
   serialize() {
-    let obj = {}
-    for (let [index, val] of Object.entries(this.constructor.FLAGS)) {
+    let obj = {};
+    for (let [index, val] of Object.entries(this.constructor.Flags)) {
       if (this.has(index)) {
-        obj[index] = val
+        obj[index] = val;
       }
     }
 
-    return obj
+    return obj;
   }
 
   freeze() {
-    return Object.freeze(this)
+    return Object.freeze(this);
   }
 
   static resolve(bit) {
-    const {defaultBit} = this
-    if (typeof defaultBit === typeof bit && bit >= defaultBit) return bit
-    if (bit instanceof Bitfield) return bit.bitfield
-    if (Array.isArray(bit)) return bit?.map((o) => this.resolve(o)).reduce((a, b) => a | b, defaultBit)
+    const {defaultBit} = this;
+    if (typeof defaultBit === typeof bit && bit >= defaultBit) return bit;
+    if (bit instanceof Bitfield) return bit.bitfield;
+    if (Array.isArray(bit)) return bit?.map((o) => this.resolve(o)).reduce((a, b) => a | b, defaultBit);
     if (typeof bit === "string") {
-      if (typeof this.FLAGS[bit] !== "undefined") return this.FLAGS[bit]
-      if (!isNaN(bit)) return typeof defaultBit === "bigint" ? BigInt(bit) : Number(bit)
+      if (typeof this.Flags[bit] !== "undefined") return this.Flags[bit];
+      if (!isNaN(bit)) return typeof defaultBit === "bigint" ? BigInt(bit) : Number(bit);
     }
 
     throw new BitfieldInvalid({
-      message: `El bifield que has especificado no es válido o no se encuentra`,
+      message: `The bifield you have specified is invalid or not found.`,
       bit: bit,
-    })
+    });
   }
 }
 
-Bitfield.FLAGS = {}
+Bitfield.Flags = {};
 
-Bitfield.defaultBit = 0n
+Bitfield.defaultBit = 0n;
 
-module.exports = Bitfield
+module.exports = Bitfield;
