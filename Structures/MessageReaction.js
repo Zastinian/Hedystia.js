3c2c8bfc4c1baf1686d815fd4072f1e8c4ac8198
Zastinian <zastinian@gmail.com>
2023-03-22 16:28:23 -0600

    docs: MessageReaction

--- Structures/MessageReaction.js
+++ Structures/MessageReaction.js
@@ -2,7 +2,20 @@ const ReactionUserManager = require("../Managers/ReactionUserManager");
 const EmojiResolver = require("../Util/EmojiResolver");
 const Base = require("../Base/base");
 const Emoji = require("./Emoji");
+/**
+ * It's a class that represents a reaction on a message
+ * @class
+ * @extends Base
+ */
 class MessageReaction extends Base {
+  /**
+   * It's a constructor for a class called Reaction.
+   * @param [data] - The data that is passed to the constructor.
+   * @param guildId - The guild ID of the message
+   * @param channelId - The channel ID of the message
+   * @param messageId - The ID of the message that the reaction is on.
+   * @param client - The client
+   */
   constructor(data = {}, guildId, channelId, messageId, client) {
     super(client);
     this.partial = data.partial ?? false;
@@ -15,12 +28,20 @@ class MessageReaction extends Base {
     this.users = new ReactionUserManager(this, this.channelId, this.message?.id, this.emoji, this.client);
   }
 
+  /**
+   * It fetches the message, gets the reaction, and returns the reaction
+   * @returns The reaction object.
+   */
   async fetch() {
     const message = await this.channel.messages.fetch(this.message);
     const reaction = message.reactions.cache.get(this.emoji?.id ?? this.emoji?.name);
     return reaction ?? null;
   }
 
+  /**
+   * It removes a reaction from a message
+   * @returns The reaction object.
+   */
   async remove() {
     const emoji = EmojiResolver.transformEmoji(this.emoji, this.client);
     await this.client.api.delete(
@@ -29,20 +50,36 @@ class MessageReaction extends Base {
     return this;
   }
 
+  /**
+   * It returns the channel object of the channel ID that is stored in the message object
+   * @returns The channel object.
+   */
   get channel() {
     return this.client.channels._add(this.channelId);
   }
 
+  /**
+   * It returns the guild object of the channel
+   * @returns The guild object.
+   */
   get guild() {
     return this.client.guilds._add(this.channelId);
   }
 
+  /**
+   * @param user - The user that was added to the channel.
+   * @returns the value of the if statement.
+   */
   _addUsers(user) {
     if (this.partial) return;
     if (this.client.user.id === user.id) this.me = true;
     this.count++;
   }
 
+  /**
+   * @param user - The user that was removed from the voice channel.
+   * @returns the value of the variable "this.me"
+   */
   _removeUsers(user) {
     if (this.partial) return;
     if (this.client.user.id === user.id) this.me = true;
