541c914261841e29e9cd76e61e4ba06495da43cf
Zastinian <zastinian@gmail.com>
2023-03-22 16:48:24 -0600

    docs: Team

--- Structures/Team.js
+++ Structures/Team.js
@@ -2,7 +2,17 @@ const {RaidenCol} = require("../Util/@Collections/RaidenCol");
 const Snowflake = require("../Util/Snowflake");
 const Base = require("../Base/base");
 const TeamMember = require("./TeamMember");
+/**
+ * It's a class that represents a team in the Discord API
+ * @class
+ * @extends Base
+ */
 class Team extends Base {
+  /**
+   * It's a constructor for a class that extends another class
+   * @param [data] - The data that was passed to the constructor
+   * @param client - The client that instantiated the object.
+   */
   constructor(data = {}, client) {
     super(client);
 
@@ -15,6 +25,13 @@ class Team extends Base {
     this.ownerUserId = this.client.users._add(data?.owner_user_id);
   }
 
+  /**
+   * "If the team has an icon, return the icon URL, otherwise return null."
+   *
+   * The function takes an optional parameter, options, which is an object
+   * @param [options] - An object containing the following properties:
+   * @returns The URL of the team icon.
+   */
   iconURL(options = {}) {
     if (!this.icon) return null;
     return this.client.cdn.TeamIcon(this.icon, options.dynamic, options.size, options.format, this.id);
