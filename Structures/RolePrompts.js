748fed332b836b2986fe656a824786d1a555a79c
Zastinian <zastinian@gmail.com>
2023-03-22 16:40:15 -0600

    docs: RolePrompts

--- Structures/RolePrompts.js
+++ Structures/RolePrompts.js
@@ -1,7 +1,18 @@
 const {RaidenCol} = require("../Util/@Collections/RaidenCol");
 const Snowflake = require("../Util/Snowflake");
 const Base = require("../Base/base");
+/**
+ * It's a class that takes in data, guildId, and client.
+ * @class
+ * @extends Base
+ */
 class RolePrompts extends Base {
+  /**
+   * It's a constructor for a class that takes in data, guildId, and client.
+   * @param [data] - The data that is being passed in.
+   * @param guildId - The guild ID
+   * @param client - The client
+   */
   constructor(data = {}, guildId, client) {
     super(client);
     this.partial = data.partial ?? false;
@@ -16,10 +27,24 @@ class RolePrompts extends Base {
     this.title = data.title ?? null;
   }
 
+  /**
+   * It returns the guild object of the guild ID that is stored in the database
+   * @returns The guild object.
+   */
   get guild() {
     return this.client.guilds._add(this.guildId);
   }
 
+  /**
+   * It takes an object with keys that are either camelCase or snake_case and returns an object with keys
+   * that are camelCase
+   * @param [roles] - The object that contains the data that needs to be transformed.
+   * @returns An object with the following properties:
+   * - emojiId
+   * - emojiName
+   * - name
+   * - rolesId
+   */
   static transformRolesData(roles = {}) {
     return {
       emojiId: roles.emojiId ?? roles.emoji_id ?? null,
