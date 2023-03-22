851cdc80e06ed01a95b0170b764dd1f36c04fadf
Zastinian <zastinian@gmail.com>
2023-03-22 16:35:21 -0600

    docs: Presence

--- Structures/Presence.js
+++ Structures/Presence.js
@@ -1,6 +1,16 @@
 const Base = require("../Base/base");
 const Activity = require("./Activity");
+/**
+ * It's a class that represents a user's presence
+ * @class
+ * @extends Base
+ */
 class Presence extends Base {
+  /**
+   * This function is a constructor for the class Presence.
+   * @param [data] - The data that was received from the API.
+   * @param client - DiscordClient
+   */
   constructor(data = {}, client) {
     super(client);
     this.partial = data.partial ?? false;
