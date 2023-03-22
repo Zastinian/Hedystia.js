8fa80a408ac0f8abf59ca7c5b46442c468fb1acb
Zastinian <zastinian@gmail.com>
2023-03-22 12:45:26 -0600

    docs: GuildVanity

--- Structures/GuildVanity.js
+++ Structures/GuildVanity.js
@@ -1,5 +1,15 @@
 const Base = require("../Base/base");
+/**
+ * It's a class that represents a guild vanity
+ * @class
+ * @extends Base
+ */
 class GuildVanity extends Base {
+  /**
+   * @param [data] - The data that is passed to the constructor.
+   * @param guildId - The ID of the guild the invite is for.
+   * @param client - The client instance
+   */
   constructor(data = {}, guildId, client) {
     super(client);
 
@@ -8,6 +18,10 @@ class GuildVanity extends Base {
     this.uses = data.uses;
   }
 
+  /**
+   * It returns the guild object of the guild ID that is stored in the database
+   * @returns The guild object.
+   */
   get guild() {
     return this.client.guilds._add(this.guildId);
   }
