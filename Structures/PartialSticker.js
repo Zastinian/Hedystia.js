15d3b952948c68169e31f8fe9f09e071b1050ed4
Zastinian <zastinian@gmail.com>
2023-03-22 16:33:11 -0600

    docs: PartialSticker

--- Structures/PartialSticker.js
+++ Structures/PartialSticker.js
@@ -1,7 +1,20 @@
 const {StickerFormatType} = require("../Util/Constants");
 const Snowflake = require("../Util/Snowflake");
 const Base = require("../Base/base");
+/**
+ * It's a class that represents a partial sticker
+ * @class
+ * @extends Base
+ */
 class PartialSticker extends Base {
+  /**
+   * This function is a constructor for the Sticker class, which is a subclass of the Base class, and it
+   * takes in a data object and a client object, and it sets the id, name, formatType, createdAt, and
+   * createdTimestamp properties of the Sticker class to the values of the id, name, format_type, id,
+   * and createdAt properties of the data object, respectively, and it returns the Sticker class.
+   * @param [data] - The data that was passed to the constructor.
+   * @param client - The client that instantiated the object.
+   */
   constructor(data = {}, client) {
     super(client);
     this.id = data.id ?? null;
@@ -11,6 +24,10 @@ class PartialSticker extends Base {
     this.createdTimestamp = this.createdAt?.getTime() ?? null;
   }
 
+  /**
+   * It fetches the sticker
+   * @returns The sticker object itself.
+   */
   async fetch() {
     return await this.client.fetchSticker(this);
   }
