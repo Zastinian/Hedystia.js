9fc0a05431631c4ae9ed1bfd9bd9f1be88e13b6d
Zastinian <zastinian@gmail.com>
2023-03-22 16:29:11 -0600

    docs: MessageReference

--- Structures/MessageReference.js
+++ Structures/MessageReference.js
@@ -1,5 +1,20 @@
 const Base = require("../Base/base");
+/**
+ * It's a class that takes in a data object and a client, and then sets the properties of the class to
+the values of the data object.
+ * @class
+ * @extends Base
+ */
 class MessageReference extends Base {
+  /**
+   * It's a constructor function that takes in two parameters, data and client, and sets the value of
+   * this.messageId to data.message_id if it exists, otherwise it sets it to data.messageId if it exists,
+   * otherwise it sets it to null.
+   *
+   * It does the same thing for the other three variables.
+   * @param [data] - The data that is passed to the constructor.
+   * @param client - The client that is sending the request
+   */
   constructor(data = {}, client) {
     super(client);
     this.messageId = data.message_id ?? data.messageId ?? null;
@@ -8,6 +23,10 @@ class MessageReference extends Base {
     this.failIfNotExists = data.fail_if_not_exists ?? data.failIfNotExists ?? null;
   }
 
+  /**
+   * It returns an object with the message ID, channel ID, guild ID, and fail if not exists
+   * @returns The message ID, channel ID, guild ID, and fail if not exists.
+   */
   toJSON() {
     return {
       message_id: this.messageId,
