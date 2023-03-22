9e3ec68697d565293468ac334281a20e564ced0a
Zastinian <zastinian@gmail.com>
2023-03-22 12:25:58 -0600

    docs: GuildPreview

--- Structures/GuildPreview.js
+++ Structures/GuildPreview.js
@@ -1,7 +1,17 @@
 const GuildEmojiManager = require("../Managers/GuildEmojiManager");
 const Snowflake = require("../Util/Snowflake");
 const Base = require("../Base/base");
+
+/**
+ * Represents the preview for a guild.
+ * @class
+ * @extends {Base}
+ */
 class GuildPreview extends Base {
+  /**
+   * @param {Object} data The data for the guild preview
+   * @param {Client} client The instantiating client
+   */
   constructor(data = {}, client) {
     super(client);
     this.id = data.id ?? null;
@@ -18,20 +28,49 @@ class GuildPreview extends Base {
     this.emojis = new GuildEmojiManager(this.id, this.client);
   }
 
+  /**
+   * Fetches the previewed guild
+   * @async
+   * @returns {Promise<GuildPreview>}
+   */
   async fetch() {
     return await this.client.fetchPreview(this);
   }
 
+  /**
+   * Returns the URL of the guild's icon
+   * @param {Object} options - Options for the icon URL
+   * @param {boolean} options.dynamic - Whether the icon should be dynamically generated
+   * @param {number} options.size - The size of the icon in pixels
+   * @param {string} options.format - The format of the icon (e.g. "png" or "jpeg")
+   * @returns {?string} The URL of the guild's icon or null if no icon is available
+   */
   iconURL(options = {}) {
     if (!this.icon) return null;
     return this.client.cdn.GuildIcon(this.icon, options.dynamic, options.size, options.format, this.id);
   }
 
+  /**
+   * Returns the URL for the guild splash image.
+   * @param {Object} [options] - Options for the splash image.
+   * @param {boolean} [options.dynamic=false] - Whether to generate a dynamic or static image.
+   * @param {number} [options.size] - The size of the image in pixels.
+   * @param {string} [options.format] - The format of the image (e.g. "webp", "png", etc.).
+   * @returns {?string} The URL for the guild splash image or null if no splash image is available.
+   */
   splashURL(options = {}) {
     if (!this.splash) return null;
     return this.client.cdn.GuildSplash(this.splash, options.dynamic, options.size, options.format, this.id);
   }
 
+  /**
+   * Returns the URL for the guild discovery splash image.
+   * @param {Object} [options] - Options for the discovery splash image.
+   * @param {boolean} [options.dynamic=false] - Whether to generate a dynamic or static image.
+   * @param {number} [options.size] - The size of the image in pixels.
+   * @param {string} [options.format] - The format of the image (e.g. "webp", "png", etc.).
+   * @returns {?string} The URL for the guild discovery splash image or null if no discovery splash image is available.
+   */
   discoverySplashURL(options = {}) {
     if (!this.discoverySplash) return null;
     return this.client.cdn.GuildDiscoverySplash(this.discoverySplash, options.dynamic, options.size, options.format, this.id);
