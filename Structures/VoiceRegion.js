15c4079e5f9242c278bfe823dde1661f5ff18f85
Zastinian <zastinian@gmail.com>
2023-03-22 16:59:55 -0600

    docs: VoiceRegion

--- Structures/VoiceRegion.js
+++ Structures/VoiceRegion.js
@@ -1,5 +1,16 @@
 const Base = require("../Base/base");
+/**
+ * It's a class that represents a Discord voice region
+ * @class
+ * @extends Base
+ */
 class VoiceRegion extends Base {
+  /**
+   * It's a constructor function that takes in data and a client, and then sets the data to the class
+   * properties
+   * @param [data] - The data that is passed to the constructor.
+   * @param client - The client that instantiated the object.
+   */
   constructor(data = {}, client) {
     super(client);
     this.id = data.id ?? null;
