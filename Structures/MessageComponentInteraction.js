50fbd0e0894ffa8d72a76a47a1e1cad9eacc19ed
Zastinian <zastinian@gmail.com>
2023-03-22 16:26:19 -0600

    docs: MessageComponentInteraction

--- Structures/MessageComponentInteraction.js
+++ Structures/MessageComponentInteraction.js
@@ -1,7 +1,17 @@
 const {ComponentType} = require("../Util/Constants");
 const MessagePayload = require("../Util/MessagePayload");
 const Interaction = require("./Interaction");
+/**
+ * It's a class that represents a message interaction
+ * @class
+ * @extends Interaction
+ */
 class MessageComponentInteraction extends Interaction {
+  /**
+   * @param [data] - The data that is passed to the constructor.
+   * @param guildId - The ID of the guild the message is in
+   * @param client - Discord.Client
+   */
   constructor(data = {}, guildId, client) {
     super(data, guildId, client);
 
@@ -11,6 +21,12 @@ class MessageComponentInteraction extends Interaction {
     this.message = this.channel.messages?._add(data.message, this.guildId, this.channelId);
   }
 
+  /**
+   * It takes an object as an argument, creates a new MessagePayload object with the options and a 6,
+   * then posts to the API with the body of the MessagePayload object.
+   * @param [options] - An object containing the following properties:
+   * @returns The response from the API.
+   */
   async deferUpdate(options = {}) {
     const body = await MessagePayload.create(options, 6);
     return await this.client.api.post(`${this.client.root}/interactions/${this.id}/${this.token}/callback`, {
@@ -18,6 +34,11 @@ class MessageComponentInteraction extends Interaction {
     });
   }
 
+  /**
+   * It updates the message with the given options and returns the reply if fetchReply is true
+   * @param [options] - An object containing the following properties:
+   * @returns The return value is the result of the await expression.
+   */
   async update(options = {}) {
     const body = await MessagePayload.create(options, 7);
     await this.client.api.post(`${this.client.root}/interactions/${this.id}/${this.token}/callback`, {body});
