2f6b40884f16ae784f502840039e5e0abf0bf155
Zastinian <zastinian@gmail.com>
2023-03-22 16:57:38 -0600

    docs: VerificationFormFields

--- Structures/VerificationFormFields.js
+++ Structures/VerificationFormFields.js
@@ -1,5 +1,16 @@
 const Base = require("../Base/base");
+/**
+ * This class is used to create a new verification form field
+ * @class
+ * @extends Base
+ */
 class VerificationFormFields extends Base {
+  /**
+   * The above function is a constructor function that takes in two parameters, data and client, and
+   * returns an object with the properties description, fieldType, label, required, and values.
+   * @param [data] - The data that is passed to the constructor.
+   * @param client - The client that instantiated the object.
+   */
   constructor(data = {}, client) {
     super(client);
     this.description = data.description ?? null;
