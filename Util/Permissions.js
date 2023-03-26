90fe56200b703e6be73f4118efe37a64e48f6c48
Zastinian <zastinian@gmail.com>
2023-03-26 10:05:48 -0600

    feat: Permissions

--- Util/Permissions.js
+++ Util/Permissions.js
@@ -61,7 +61,6 @@ Permissions.Flags = {
   Send_Messages_In_Threads: 1n << 38n,
   Use_Embedded_Activities: 1n << 39n,
   Moderate_Members: 1n << 40n,
-  View_Creator_Monetization_Analytics: 1n << 41n,
 };
 
 /**
